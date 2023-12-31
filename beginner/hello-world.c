cbdba3eda68dfa1371f86382c13224e74e00e179
Muhammad Ahmod <53302444+Muhammad-1990@users.noreply.github.com>
2023-12-31 19:03:18 +0000

    Implemented basic Hello World program in C

---
+++ beginner/hello-world.c
@@ -0,0 +1,10 @@
+// A simple C program to display "Hello, World!" on the screen.
+
+#include <stdio.h>
+
+int main()
+{
+    // printf() displays the string inside quotation
+    printf("Hello, World!");
+    return 0;
+}
